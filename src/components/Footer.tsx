0bf122a383728905f6bcf9a8a279a8b7c4c8ea9b
Michael Teye <michaelteyegyemah@gmail.com>
2023-12-16 07:31:42 +0000

    all

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,7 +80,7 @@ const Footer = () => {
                         to="#"
                         className=" hover:text-gray-900"
                     >
-                        Molad e Konsult
+                        PolicyCON
                     </HashLink>. All rights reserved.
                     </div>
                 </div>
